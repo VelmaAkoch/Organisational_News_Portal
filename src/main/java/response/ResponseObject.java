156784f08436ac1558d0964d65a8d1d56accca20
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:27:02 +0300

    add ResponseObject constructor of status and message

--- src/main/java/response/ResponseObject.java
+++ src/main/java/response/ResponseObject.java
@@ -1,4 +1,12 @@
 package response;
 
 public class ResponseObject {
+    private int status;
+    private String message;
+    private Object data;
+
+    public ResponseObject(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
 }
