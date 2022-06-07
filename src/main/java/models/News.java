38d8204c952efa985362509f0d34753a099f4f59
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 21:54:30 +0300

    add news contructor title and content

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -1,4 +1,13 @@
 package models;
 
 public class News {
+    private int id;
+    private String title;
+    private String content;
+    private int department_id;
+
+    public News(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
