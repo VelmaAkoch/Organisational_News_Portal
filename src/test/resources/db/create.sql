764ce8b82cfebdcd2768dabe3a13c59b3b100e19
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-08 01:58:40 +0300

    add db create.sql

---
+++ src/test/resources/db/create.sql
@@ -0,0 +1,26 @@
+ CREATE DATABASE Organisational_News_PortalAPI;
+ \c Organisational_News_PortalAPI
+
+ CREATE TABLE IF NOT EXISTS departments (
+    id serial PRIMARY KEY,
+    name varchar,
+    description varchar,
+    size varchar
+ );
+
+ CREATE TABLE IF NOT EXISTS users(
+ id serial PRIMARY KEY,
+ name varchar,
+ position varchar,
+ role varchar,
+ department_id int
+ );
+
+ CREATE TABLE IF NOT EXISTS news(
+ id serial PRIMARY KEY,
+ title varchar,
+ content varchar,
+ department_id int
+ );
+
+ CREATE DATABASE organisational_news_test WITH TEMPLATE organisational_news;
