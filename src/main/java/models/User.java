b44702baf143f35f6f9bf3caad4280980771bc08
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 21:44:18 +0300

    add user getters and setters

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -15,5 +15,51 @@ public class User {
         this.department_id = department_id;
     }
 
+    public User(int id, String name, String position, String role, int department_id) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.department_id = department_id;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
 }
