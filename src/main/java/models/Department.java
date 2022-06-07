7738bd32bf52887faddb581dedaff09b8a8b0f38
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:08:38 +0300

    add department getters and setters generated of id,name,description and size generated

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -7,8 +7,41 @@ public class Department {
         private int size;
 
     public Department(String name, String description, int size) {
+        this.id = id;
         this.name = name;
         this.description = description;
+        this.size = size;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
         this.size = size;
     }
 }
