0fa94f05db3a6c1efae0401be3c9f7b28ad856a1
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:21:12 +0300

    add ResponseArray generated gettres and setters of status,message and status

--- src/main/java/response/ResponseArray.java
+++ src/main/java/response/ResponseArray.java
@@ -11,4 +11,28 @@ public class ResponseArray {
         this.message = message;
         this.data = data;
     }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<Object> getData() {
+        return data;
+    }
+
+    public void setData(List<Object> data) {
+        this.data = data;
+    }
 }
