db5d13cec9c25c8acc958bd6faf05019bc116b21
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 21:57:58 +0300

    add nws generated getters and setters of id,title,content and department_id

--- src/main/java/models/News.java
+++ src/main/java/models/News.java
@@ -10,4 +10,43 @@ public class News {
         this.title = title;
         this.content = content;
     }
+
+    public News(int id, String title, String content, int department_id) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.department_id = department_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getDepartment_id() {
+        return department_id;
+    }
+
+    public void setDepartment_id(int department_id) {
+        this.department_id = department_id;
+    }
 }
