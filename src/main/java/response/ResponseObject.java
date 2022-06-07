7300773687f9026fddceb795ed6ee5b563b4f9ae
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:29:26 +0300

    add ResponseObject getters and status of object,status and message generated

--- src/main/java/response/ResponseObject.java
+++ src/main/java/response/ResponseObject.java
@@ -9,4 +9,28 @@ public class ResponseObject {
         this.status = status;
         this.message = message;
     }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
