fd70aafecaf06f895159f031fafb40916b60ff22
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-08 01:45:26 +0300

    add class Department Test

--- src/test/java/models/DepartmentTest.java
+++ src/test/java/models/DepartmentTest.java
@@ -1,4 +1,41 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 public class DepartmentTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getSize() {
+    }
+
+    @Test
+    void setSize() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
 }
