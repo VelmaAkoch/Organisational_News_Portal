51b8bde787210bb77d6881b9287317e543fad199
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-08 00:00:50 +0300

    add class News Test

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,4 +1,45 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 public class NewsTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void getContent() {
+    }
+
+    @Test
+    void setContent() {
+    }
+
+    @Test
+    void getDepartment_id() {
+    }
+
+    @Test
+    void setDepartment_id() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
 }
