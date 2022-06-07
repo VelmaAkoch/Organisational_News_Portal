420186365544e501e1a80c65346dcae3ca56a0a8
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 23:59:39 +0300

    add class User Test

--- src/test/java/models/UserTest.java
+++ src/test/java/models/UserTest.java
@@ -1,4 +1,53 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getPosition() {
+    }
+
+    @Test
+    void setPosition() {
+    }
+
+    @Test
+    void getRole() {
+    }
+
+    @Test
+    void setRole() {
+    }
+
+    @Test
+    void getDepartment_id() {
+    }
+
+    @Test
+    void setDepartment_id() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
 }
