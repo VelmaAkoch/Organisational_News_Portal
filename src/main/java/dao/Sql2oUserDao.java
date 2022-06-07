f8f0f7ea287da4a5219a4f4888e7cc76ff611700
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:45:46 +0300

    add overide method of user implemeted

--- src/main/java/dao/Sql2oUserDao.java
+++ src/main/java/dao/Sql2oUserDao.java
@@ -1,4 +1,45 @@
 package dao;
 
-public class Sql2oUserDao {
+import models.User;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oUserDao implements  UserDao{
+    private final Sql2o sql2o;
+    public  Sql2oUserDao(Sql2o sql2o){
+        this.sql2o=sql2o;
+    }
+
+    @Override
+    public void add(User user) {
+        String sql = "INSERT INTO users (name, position, role, department_id) VALUES (:name, :position, :role, :department_id)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+
+    }
+
+//    @Override
+//    public List<User> getAll() {
+//        return null;
+//    }
+//
+//    @Override
+//    public User findById(int id) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<User> getAllUsersByDepartment(int user_id) {
+//        return null;
+//    }
 }
