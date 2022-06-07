caf7155946adec9ef2ba8ec31208aed4d5338492
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 21:39:19 +0300

    add user contructor name,position and role

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,19 @@
 package models;
 
 public class User {
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private int department_id;
+
+    public User(String name, String position, String role) {
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.id = id;
+        this.department_id = department_id;
+    }
+
+
 }
