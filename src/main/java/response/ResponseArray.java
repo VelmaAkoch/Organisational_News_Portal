f7071cfd5aac77090568facdec1c88e3b8bc7379
VelmaAkoch <velmaakochi1@gmail.com>
2022-06-07 22:17:54 +0300

    add ResponseArray contructor of status and message

--- src/main/java/response/ResponseArray.java
+++ src/main/java/response/ResponseArray.java
@@ -1,4 +1,14 @@
 package response;
 
+import java.util.List;
+
 public class ResponseArray {
+    private int status;
+    private String message;
+    private List<Object> data;
+
+    public ResponseArray(String message, List<Object> data) {
+        this.message = message;
+        this.data = data;
+    }
 }
